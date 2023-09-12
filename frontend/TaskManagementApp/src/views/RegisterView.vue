2deeed189a67404bced83bd7f8d6c9951a4d29ef
PacMan111 <luizgustavossj@gmail.com>
2023-09-12 08:39:19 -0400

    feat(RegisterView): Updated the register interface Co-authored-by: Geffté Caetano <gefft3@gmail.com>

--- frontend/TaskManagementApp/src/views/RegisterView.vue
+++ frontend/TaskManagementApp/src/views/RegisterView.vue
@@ -1,5 +1,4 @@
 <script setup>
-
 import {reactive} from 'vue'
 import router from "@/router";
 
@@ -24,17 +23,41 @@ function register(){
 </script>
 
 <template>
-    <section>
-      <label>Email: </label> 
-      <input type="text" placeholder="Email" v-model="user.email" required>
-      <label>Senha:</label>
-      <input type="text" placeholder="Senha" v-model="user.password" required>
-      <label>Confirme a senha:</label>
-      <input type="text" placeholder="Confirmar senha" required>
-      <label>Nome de usuário:</label>
-      <input type="text" placeholder="Nome de usuário" v-model="user.username" required>
-      <label>Nome:</label>
-      <input type="text" placeholder="Nome" v-model="user.name" required>
-      <button @click="register">Cadastrar</button>
-    </section>
+  <section class="bg-gray-50 dark:bg-gray-900 fixed top-0 left-0 w-screen h-screen">
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Crie sua conta
+              </h1>
+              <form class="space-y-4 md:space-y-6" action="#">
+                  <div>
+                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                      <input v-model="user.email" type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="email@exemplo.com" required="">
+                  </div>
+                  <div>
+                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nome</label>
+                      <input v-model="user.name" type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Seu nome" required="">
+                  </div>
+                  <div>
+                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nome de usuário</label>
+                      <input v-model="user.username" type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Seu nome de usuário" required="">
+                  </div>
+                  <div>
+                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Senha</label>
+                      <input v-model="user.password" type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                  </div>
+                  <div>
+                      <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirme sua senha</label>
+                      <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                  </div>
+                  <button @click="register" type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Cria sua conta</button>
+                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                      Já tem conta??? <router-link class="text-white hover:text-blue-500" to="/login">Logue aqui</router-link>
+                    </p>
+              </form>
+          </div>
+      </div>
+    </div>
+  </section>
 </template>
